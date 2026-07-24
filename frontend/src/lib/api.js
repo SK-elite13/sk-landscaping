fa724eee1f402d4c76c9470f4db7fe5663465eb2
SK-elite13 <sklandscaping.in@gmail.com>
2026-07-24 12:17:10 +0530

    Create api.js

---
+++ frontend/src/lib/api.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "";
+export const API = `${BACKEND_URL}/api`;
+export const api = axios.create({ baseURL: API });
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("sk_token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+export const CONTACT = {
+  phone: "+91 93130 82732",
+  phoneRaw: "+919313082732",
+  whatsapp: "919313082732",
+  email: "sklandscaping.in@gmail.com",
+  address: "Ode, Anand, Gujarat, India",
+};
+
+export const waLink = (msg = "Hi SK Landscaping, I'd like to know more about your services.") =>
+  `https://wa.me/${CONTACT.whatsapp}?text=${encodeURIComponent(msg)}`;
