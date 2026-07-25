dbd77ffb900cddb7eb10ec70ff29b2d3c16099a2
SK-elite13 <sklandscaping.in@gmail.com>
2026-07-25 15:40:17 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,6 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import { LeadDialogProvider } from "./context/LeadDialogContext";
-import { PublicLayout } from "./components/PublicLayout";
-import { LeadDialog } from "./components/LeadDialog";
 
 import Home from "./pages/Home";
 import ServicesPage from "./pages/ServicesPage";
@@ -16,19 +13,16 @@ export default function App() {
   return (
     <AuthProvider>
       <LeadDialogProvider>
-        <Router>
-          <PublicLayout>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/services" element={<ServicesPage />} />
-              <Route path="/projects" element={<ProjectsPage />} />
-              <Route path="/about" element={<AboutPage />} />
-              <Route path="/contact" element={<ContactPage />} />
-              <Route path="/admin/login" element={<AdminLogin />} />
-            </Routes>
-          </PublicLayout>
-          <LeadDialog />
-        </Router>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/services" element={<ServicesPage />} />
+            <Route path="/projects" element={<ProjectsPage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/admin/login" element={<AdminLogin />} />
+          </Routes>
+        </BrowserRouter>
       </LeadDialogProvider>
     </AuthProvider>
   );
