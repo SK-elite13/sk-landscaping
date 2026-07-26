93c0adc4c5ca5ad7a10dd475bf781372c8a56218
SK-elite13 <sklandscaping.in@gmail.com>
2026-07-26 11:25:15 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { PublicLayout } from "./components/PublicLayout";
 import { LeadDialogProvider } from "./context/LeadDialogContext";
+
 import Home from "./pages/Home";
+// Make sure these page files exist in src/pages/ or standard components
+import { Services } from "./pages/Services"; 
+import { Projects } from "./pages/Projects";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
 
 export default function App() {
   return (
@@ -11,6 +17,10 @@ export default function App() {
         <PublicLayout>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
           </Routes>
         </PublicLayout>
       </Router>
