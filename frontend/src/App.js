d03ece292997c172385ead9858f4784ae003b7e3
SK-elite13 <sklandscaping.in@gmail.com>
2026-07-26 11:18:32 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { PublicLayout } from "./components/PublicLayout";
 import { LeadDialogProvider } from "./context/LeadDialogContext";
-
 import Home from "./pages/Home";
-import ServicesPage from "./pages/ServicesPage";
-import ProjectsPage from "./pages/ProjectsPage";
-import AboutPage from "./pages/AboutPage";
-import ContactPage from "./pages/ContactPage";
-import AdminLogin from "./pages/AdminLogin";
 
 export default function App() {
   return (
-    <AuthProvider>
-      <LeadDialogProvider>
-        <BrowserRouter>
+    <LeadDialogProvider>
+      <Router>
+        <PublicLayout>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/services" element={<ServicesPage />} />
-            <Route path="/projects" element={<ProjectsPage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/contact" element={<ContactPage />} />
-            <Route path="/admin/login" element={<AdminLogin />} />
           </Routes>
-        </BrowserRouter>
-      </LeadDialogProvider>
-    </AuthProvider>
+        </PublicLayout>
+      </Router>
+    </LeadDialogProvider>
   );
 }
