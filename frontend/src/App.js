5b14b8e74dd86ba196536b8633e465db3a1dd20b
SK-elite13 <sklandscaping.in@gmail.com>
2026-07-28 13:10:19 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { PublicLayout } from "./components/PublicLayout";
 import { LeadDialogProvider } from "./context/LeadDialogContext";
+import ScrollToTop from "./components/ScrollToTop"; // Added ScrollToTop helper
 
 // Default imports matching 'export default ...' in your page files
 import Home from "./pages/Home";
@@ -14,6 +15,7 @@ export default function App() {
   return (
     <LeadDialogProvider>
       <Router>
+        <ScrollToTop /> {/* Reset scroll position on route change */}
         <PublicLayout>
           <Routes>
             <Route path="/" element={<Home />} />
