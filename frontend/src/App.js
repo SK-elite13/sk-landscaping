744cacc5e60838c3ac4562d038e176deb8669144
SK-elite13 <sklandscaping.in@gmail.com>
2026-07-29 17:34:43 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,43 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import { PublicLayout } from "./components/PublicLayout";
 import { LeadDialogProvider } from "./context/LeadDialogContext";
-import ScrollToTop from "./components/ScrollToTop"; // Added ScrollToTop helper
+import ScrollToTop from "./components/ScrollToTop";
 
-// Default imports matching 'export default ...' in your page files
+// Page Imports
 import Home from "./pages/Home";
 import ServicesPage from "./pages/ServicesPage";
 import ProjectsPage from "./pages/ProjectsPage";
 import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
 
+// Wrapper component to apply smooth fade & slide-up animation on route change
+function AnimatedRoutes() {
+  const location = useLocation();
+
+  return (
+    <div
+      key={location.pathname}
+      className="animate-fade-slide-up transition-all duration-300 ease-out"
+    >
+      <Routes location={location}>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<ServicesPage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+    </div>
+  );
+}
+
 export default function App() {
   return (
     <LeadDialogProvider>
       <Router>
-        <ScrollToTop /> {/* Reset scroll position on route change */}
+        <ScrollToTop />
         <PublicLayout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/services" element={<ServicesPage />} />
-            <Route path="/projects" element={<ProjectsPage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/contact" element={<ContactPage />} />
-          </Routes>
+          <AnimatedRoutes />
         </PublicLayout>
       </Router>
     </LeadDialogProvider>
